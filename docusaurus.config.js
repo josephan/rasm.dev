cd5a0834622308b28ed52f7c3557fcbdd2d52818
Joseph An <an.joseph891@gmail.com>
2021-10-04 05:06:07 -0400

    update config and CNAME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   favicon: 'img/favicon.ico',
   organizationName: 'josephan', // Usually your GitHub org/user name.
   projectName: 'rasm.dev', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
